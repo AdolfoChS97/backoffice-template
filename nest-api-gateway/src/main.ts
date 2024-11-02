8c0b8549a61e6a37d71c07dc6f2834052a195c1d
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-11-02 18:54:24 -0400

    feature:[enable cors of api-gateway ]

--- nest-api-gateway/src/main.ts
+++ nest-api-gateway/src/main.ts
@@ -5,6 +5,12 @@ import IAppConfigVariables from './interfaces/config.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   const configService =
     app.get<ConfigService<IAppConfigVariables>>(ConfigService);
   await app.listen(configService.get<number>('APP_PORT'));
