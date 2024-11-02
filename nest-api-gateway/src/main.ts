734cc758b42dabf5a252fa47d5a3d3179e1c749d
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-11-02 19:02:01 -0400

    feature:[add swagger]

--- nest-api-gateway/src/main.ts
+++ nest-api-gateway/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import IAppConfigVariables from './interfaces/config.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+  const config = new DocumentBuilder()
+    .setTitle('API Gateway example')
+    .setDescription('The API gateway API description')
+    .setVersion('1.0')
+    .addTag('API Gateway')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   const configService =
     app.get<ConfigService<IAppConfigVariables>>(ConfigService);
   await app.listen(configService.get<number>('APP_PORT'));
