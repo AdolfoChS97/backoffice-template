42283d79788f90014202ac9986652f618e4aad08
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-11-02 18:50:46 -0400

    feature:[add config module]

--- nest-api-gateway/src/main.ts
+++ nest-api-gateway/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import IAppConfigVariables from './interfaces/config.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService =
+    app.get<ConfigService<IAppConfigVariables>>(ConfigService);
+  await app.listen(configService.get<number>('APP_PORT'));
 }
 bootstrap();
